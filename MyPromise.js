0e43d74260eab1e83b8be8c4bb7718bc1471c777
hafsa-aarifeen <hafsama-im19042@stu.kln.ac.lk>
2026-01-07 11:30:57 +0530

    then

--- MyPromise.js
+++ MyPromise.js
@@ -1,4 +1,15 @@
+const STATE = {
+  FULFILLED: "fulfilled",
+  REJECTED: "rejected",
+  PENDING: "pending",
+};
+
 class Mypromise {
+  #thenCbs = [];
+  #catchCbs = [];
+  #state = STATE.PENDING;
+  #value;
+
   constructor(cb) {
     try {
       cb(this.#onSuccess, this.#onFail);
@@ -7,12 +18,50 @@ class Mypromise {
     }
   }
 
-  #onSuccess(value) {}
+  #runCallbacks() {
+    if (this.#state === STATE.FULFILLED) {
+      this.#thenCbs.forEach((callback) => {
+        callback(this.#value);
+      });
 
-  #onFail(value) {}
+      this.#thenCbs = [];
+    }
+
+    if (this.#state === STATE.REJECTED) {
+      this.#catchCbs.forEach((callback) => {
+        callback(this.#value);
+      });
+
+      this.#catchCbs = [];
+    }
+  }
+
+  #onSuccess(value) {
+    if (this.#state !== STATE.PENDING) return;
+    this.#value = value;
+    this.#state = STATE.FULFILLED;
+    this.#runCallbacks();
+  }
+
+  #onFail(value) {
+    if (this.#state !== STATE.PENDING) return;
+    this.#value = value;
+    this.#state = STATE.REJECTED;
+    this.#runCallbacks();
+  }
+
+  then(cb) {
+    this.#thenCbs.push(cb);
+
+    this.#runCallbacks();
+
+    return new Promise();
+  }
 }
 
 module.exports = Mypromise;
 
-const p = new Promise(cb);
-p.then(() => {}).catch(() => {});
+const p = new Mypromise((resolve, reject) => {
+  resolve(500);
+}).then();
+p.then().then().catch();
